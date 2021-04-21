f09fa6bb14c38f5d42ac491eec7b446bc135fbe1
Seb Carss <seb.carss@theverygroup.com>
2021-04-21 09:28:45 +0100

    Use stubbed data for product page

--- pages/products/[pid].js
+++ pages/products/[pid].js
@@ -1,16 +1,16 @@
 import Grid from '@material-ui/core/Grid'
 
 export default function Product({ data }) {
-    // let nav = data.nav
-    // let images = data.images
+    let nav = data.nav
+    let images = data.images
 
     // TODO Pass the data to the appropriate layout to render
 
     console.log({data})
     return (
         <div>
-                ID: {data.id}
-            {/* <div>
+                {/* ID: {data.id} */}
+            <div>
                 <img src={images.hero_image} alt="picture of awesome sofa"></img>
             </div>
 
@@ -20,7 +20,7 @@ export default function Product({ data }) {
             <div>{data.price}</div>
             <div>
                 <button id="addToBasketButton">Add to Basket</button>
-            </div> */}
+            </div>
         </div>
     )
 }
@@ -29,37 +29,41 @@ export async function getServerSideProps({ params }) {
 
     // TODO Call product API to get product data including nav categories
 
-    // const data = {
-    //     "id": "1",
-    //     "nav": {
-    //         "department": "home_and_furniture",
-    //         "category": "sofas",
-    //         "subcategory": "corner_sofas",
-    //     },
-    //     "images": {
-    //         "hero_image": "https://media.very.co.uk/i/very/MCTQN_SQ1_0000011520_VINTAGE_TAN_SLf?$550x733_standard$",
-    //     },
-    //     "price": "£1499",
-    //     "title": "Hampshire Premium Leather Corner Group Sofa"
-    // }
+    const data = {
+        "id": "1",
+        "nav": {
+            "department": "home_and_furniture",
+            "category": "sofas",
+            "subcategory": "corner_sofas",
+        },
+        "images": {
+            "hero_image": "https://media.very.co.uk/i/very/MCTQN_SQ1_0000011520_VINTAGE_TAN_SLf?$550x733_standard$",
+        },
+        "price": "£1499",
+        "title": "Hampshire Premium Leather Corner Group Sofa"
+    }
+
+    return {
+        props: { data }
+    }
 
     // TODO Call Amplience to get layout information
 
-    const productId = params.pid
-    console.log(productId)
+    // const productId = params.pid
+    // console.log(productId)
 
-    const res = await fetch(`https://www.very.co.uk/api/selling-product/selling-products/${productId}?source=atg`)
-    const data = await res.json()
+    // const res = await fetch(`https://www.very.co.uk/api/selling-product/selling-products/${productId}?source=atg`)
+    // const data = await res.json()
 
-    console.log(data);
+    // console.log(data);
 
-    if (!data) {
-        return {
-            notFound: true,
-        }
-    }
+    // if (!data) {
+    //     return {
+    //         notFound: true,
+    //     }
+    // }
     
-    return {
-        props: { data} // will be passed to the page component as props
-    }
+    // return {
+    //     props: { data} // will be passed to the page component as props
+    // }
 }
